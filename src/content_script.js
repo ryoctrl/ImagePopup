81c5074802cca07dcc50e36b2356455eb9ea7baa
ryoctrl <ryoctrl@gmail.com>
2019-06-21 02:37:46 +0900

    impl

--- src/content_script.js
+++ src/content_script.js
@@ -1,11 +1,29 @@
-﻿//----------------------
-$(function () {
-    //↓疎通確認コード：タイトルの■が回転するはず
-    console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
-    let flag = false;
-    setInterval(function () {
-        document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
-        flag = !flag;
-    }, 1000);
-    //↑疎通確認コード
+﻿$(function() {
+    let url = '';
+    $('html').mousemove(e => {
+        const classes = e.target.className;
+
+        if (!classes) {
+            url = '';
+            return;
+        }
+
+        if (classes.indexOf('js-media-image-link') !== -1) {
+            const style = e.target.style;
+            if (!style.backgroundImage) return;
+            const fullPath = style.backgroundImage.split('"');
+            const urlWithQuery = fullPath[1];
+            url = (urlWithQuery.split('?'))[0] + ':orig';
+        } else if (classes.indexOf('media-img') !== -1 || classes.indexOf('media-image') !== -1) {
+            const src = e.target.src;
+            url = src.replace('large', 'orig');
+        } else return;
+    });
+
+    $('html').keydown(e => {
+        console.log('opening to ', url);
+        if (url === '' || e.keyCode !== 17) return;
+        window.open(url);
+        url = '';
+    });
 });
